4e591d9f9f1d78927cd2401b3688548fc7ab5801
bebzbzbz <balzerbeatrice@gmail.com>
2024-12-13 13:38:00 +0100

    Abschlussprojekt

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,6 @@ function calc() {
     let age = Number(document.querySelector("#age").value);
     let weight = Number(document.querySelector("#weight").value);
     let activity = Number(document.querySelector("#activity").value);
-    console.log(activity);
     let gender = document.querySelector("input[name=gender]:checked").value;
 
     let basalKcal = document.querySelector("#basal-kcal");
@@ -11,17 +10,32 @@ function calc() {
     let totalKcal = document.querySelector("#total-kcal");
     let totalKj = document.querySelector("#total-kj");
 
-    if(gender = "female" && bodySize > 0 && age > 0 && weight > 0) {
-        basalKcal.innerText = Math.ceil(66.47 + (13.7 * weight) + (5 * bodySize) - (6.8 * age));
-        totalKcal.innerText = Math.ceil(Number(basalKcal.innerText) * activity);
-
-        basalKj.innerText = Math.ceil(Number(basalKcal.innerText) * 4.184);
-        totalKj.innerText = Math.ceil(Number(totalKcal.innerText) * 4.184);
-    } else {
+    if(gender == "female") {
         basalKcal.innerHTML = Math.ceil(655.1 + (9.6 * weight) + (1.8 * bodySize) - (4.7 * age));
         totalKcal.innerText = Math.ceil(Number(basalKcal.innerText) * activity);
 
         basalKj.innerText = Math.ceil(Number(basalKcal.innerText) * 4.184);
         totalKj.innerText = Math.ceil(Number(totalKcal.innerText) * 4.184);
+    } else {
+        basalKcal.innerText = Math.ceil(66.47 + (13.7 * weight) + (5 * bodySize) - (6.8 * age));
+        totalKcal.innerText = Math.ceil(Number(basalKcal.innerText) * activity);
+
+        basalKj.innerText = Math.ceil(Number(basalKcal.innerText) * 4.184);
+        totalKj.innerText = Math.ceil(Number(totalKcal.innerText) * 4.184);
     }
+}
+
+let monthly = document.querySelector(".pricing .btns-container a:first-of-type");
+let yearly = document.querySelector(".pricing .btns-container a:nth-of-type(2");
+
+function btnChange1() {
+    monthly.classList.remove("active");
+    monthly.classList.add("secondary");
+    yearly.classList.add("active");
+}
+
+function btnChange2() {
+    yearly.classList.remove("active");
+    yearly.classList.add("secondary");
+    monthly.classList.add("active");
 }
